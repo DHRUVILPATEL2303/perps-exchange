fd86bde29c296e3e41923891c0cfeb9a3247c08d
DHRUVILPATEL2303 <pateldhruvil2303@gmail.com>
2026-08-04 00:54:34 +0530

    feat : add pgbouncer as connection pool proxy

--- crates/database/src/pool.rs
+++ crates/database/src/pool.rs
@@ -1,8 +1,16 @@
-use sqlx::{PgPool, postgres::PgPoolOptions};
+use sqlx::{PgPool, postgres::PgPoolOptions, postgres::PgConnectOptions};
+use std::str::FromStr;
 
 pub async fn create_pool(config: &config::DatabaseConfig) -> Result<PgPool, sqlx::Error> {
+    let mut connect_options = PgConnectOptions::from_str(&config.url().to_string())?;
+    connect_options = connect_options.statement_cache_capacity(0);
+
     PgPoolOptions::new()
         .max_connections(80)
-        .connect(&config.url().to_string())
+        .min_connections(20)
+        .acquire_timeout(std::time::Duration::from_secs(3))
+        .idle_timeout(std::time::Duration::from_secs(60))
+        .test_before_acquire(false)
+        .connect_with(connect_options)
         .await
 }
